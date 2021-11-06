f41bc312b68ae51a7daf2c276402c44d29f89d52
brentnicks <brent_nicks@msn.com>
2021-11-06 16:13:12 -0700

    413

--- New Unity Project/Assets/Scripts/ObjectPhysics.cs
+++ New Unity Project/Assets/Scripts/ObjectPhysics.cs
@@ -8,7 +8,14 @@ public class ObjectPhysics : MonoBehaviour
     {
         if (collision.gameObject.tag == "Falling Object")
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(100,100));
+            collision.gameObject.GetComponent<Rigidbody2D>().drag = 20;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Falling Object")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().drag = 0;
         }
     }
 }
