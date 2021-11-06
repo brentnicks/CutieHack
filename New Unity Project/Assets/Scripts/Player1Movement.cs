77484aeef1a292a88d47400e956a424825120329
PotatoToast <92888064+PotatoToast@users.noreply.github.com>
2021-11-06 13:08:37 -0700

    108

--- New Unity Project/Assets/Scripts/Player1Movement.cs
+++ New Unity Project/Assets/Scripts/Player1Movement.cs
@@ -6,24 +6,24 @@ public class Player1Movement : MonoBehaviour
 {
     Rigidbody2D rb;
     public float horizontalSpeed;
+<<<<<<< Updated upstream
+=======
+    bool canJump = true;
+    private Vector3 m_Velocity = Vector3.zero;
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
         rb = GameObject.Find("Player1").GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = new Vector3(horizontalSpeed, rb.velocity.y, 0);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            rb.velocity = new Vector3(0, 0, 0);
-        }
+        Vector3 targetVelocity = new Vector2(horizontalSpeed * 10f * Input.GetAxisRaw("Horizontal1"), rb.velocity.y);
+        // And then smoothing it out and applying it to the character
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, 0.05f);
 
+<<<<<<< Updated upstream
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector3(-horizontalSpeed, rb.velocity.y, 0);
@@ -33,4 +33,17 @@ public class Player1Movement : MonoBehaviour
             rb.velocity = new Vector3(0, 0, 0);
         }
     }
+=======
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && canJump)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, 500));
+            canJump = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            canJump = true;
+    }
+>>>>>>> Stashed changes
 }
---
+++ New Unity Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawnees;
+    public GameObject box;
+    double timer;
+    public double spawnDelay;
+
+
+    int randomInt;
+    float Range;
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= spawnDelay)
+        {
+            spawnRandom();
+            timer = 0;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    void spawnRandom()
+    {
+        randomInt = Random.Range(0, spawnees.Length);
+        Range = Random.Range(-7.0f, 7.0f);
+        Instantiate(box, new Vector3 (gameObject.transform.position.x + Range, gameObject.transform.position.y, gameObject.transform.position.z) , gameObject.transform.rotation);
+    }
+}
